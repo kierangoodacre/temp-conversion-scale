5d5169dfb47dc4c196010f4f0e698683e0b6fca4
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-04-09 16:18:07 +0100

    createed Celcius passing test

--- spec/tempConversionSpec.js
+++ spec/tempConversionSpec.js
@@ -6,6 +6,14 @@ describe ('Temp conversion', function(){
       expect(convertTemp(-30, "De", "K")).toEqual(393);
     });
 
+    it('Celcius', function(){
+      expect(convertTemp(-30, "De", "C")).toEqual(120);
+    });
+
+    it('Fahrenhite', function(){
+      expect(convertTemp(-30, "De", "F")).toEqual(120);
+    });
+
   });
 
 });
--- src/tempConversion.js
+++ src/tempConversion.js
@@ -2,6 +2,10 @@ function convertTemp(temp, from_scale, to_scale){
   if (from_scale === "De"){
     if (to_scale === "K"){
       return Math.round((373.15 - (temp * (2/3))));
+    } else if (to_scale === "C"){
+      return Math.round(100-(temp * (2/3)));
+    } else if {
+
     }
   }
-}
+};
